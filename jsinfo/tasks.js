abb0547b1457369dad20010011f89e95889ac4b9
Zidan Islam <zidantwinkle@gmail.com>
2025-02-22 23:58:14 +0600

    a few basic tasks are done

---
+++ jsinfo/tasks.js
@@ -0,0 +1,30 @@
+// task-1
+alert("I'm JavaScript");
+
+// task-3
+let admin;
+let names;
+names = "John";
+admin = names;
+
+alert(admin)
+
+// task-4
+
+const ourPlanet = "Earth";
+let currentUserName = "John";
+
+//task-5
+
+let name = "Ilya";
+
+alert(`hello ${1}`); // Hello 1
+
+alert(`hello ${"name"}`); // Hello name
+
+alert(`hello ${name}`); // Hello Ilya
+
+// task-6
+
+let currentVisitorName = prompt("What is your name", "John");
+alert(`Your name is ${currentUserName}`);
--- objects/objects.js
+++ objects/objects.js
@@ -17,3 +17,5 @@ for (guy in person){
 }
 document.getElementById("sec").innerHTML = text;
 
+//Constructing a new object
+
