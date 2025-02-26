72dbc9ee825f790046a2a48fd1da0c1ef917a40a
Zidan Islam <zidantwinkle@gmail.com>
2025-02-26 16:56:27 +0600

    comparison task finished

--- jsinfo/tasks.js
+++ jsinfo/tasks.js
@@ -71,10 +71,10 @@ let task_b = prompt("Second number?", 2);
 alert(+task_a + +task_b); // 3
 
 // task-11
-5 > 4
-"apple" > "pineapple"
-"2" > "12"
-undefined == null
-undefined === null
-null == "\n0\n"
-null === +"\n0\n"
+5 > 4 //true
+"apple" > "pineapple" //false
+"2" > "12" //true
+undefined == null //true
+undefined === null // false
+null == "\n0\n" // false
+null === +"\n0\n" // false
