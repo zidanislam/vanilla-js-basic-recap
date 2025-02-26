84bfaeddc75b956861d9782f515179d87b3aebb4
Zidan Islam <zidantwinkle@gmail.com>
2025-02-26 23:23:29 +0600

    if and conditionla task compelete

--- jsinfo/tasks.js
+++ jsinfo/tasks.js
@@ -78,3 +78,51 @@ undefined == null //true
 undefined === null // false
 null == "\n0\n" // false
 null === +"\n0\n" // false
+
+// if & conditional operation
+
+let age = prompt("How old are you?", "");
+let message = (age < 3) ? "Hi baby" :
+ (age < 18) ? "Greetings" : 
+ (age < 90) ? "Hello" : "What an unusual age!";
+
+ alert(message);
+
+//  task-12
+
+let answer = prompt("What is the “official” name of JavaScript?", "");
+if (answer == "ECMAScript"){
+    alert("You are right");
+} else if(answer == "ecmacript") {
+    alert("You are right but check the spelling next time");
+} else if(answer == "Ecmascript") {
+    alert("You are right but check the spelling next time");
+}else {
+    alert("You don’t know? ECMAScript!")
+}
+
+// task-13
+
+let number = prompt("Type a number", 0);
+
+if (number > 0) {
+    alert(1)
+} else if(number < 0){
+    alert(-1)
+}else{
+    alert(0)
+}
+
+// task-14
+
+let result; 
+(a+b <4) ? result = "Below" : result = "Over";
+
+// task-15
+let messages = (login == 'Employee') ? 
+    'Hello' :
+(login == 'Director') ?
+    'Greetings' :
+(login == '') ?
+    'No login' : 
+    '';
