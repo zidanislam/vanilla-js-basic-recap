f252469e5762248a63930d1fc8a712818aa49c9a
Zidan Islam <zidantwinkle@gmail.com>
2025-03-01 04:57:35 +0600

    more tasks done

--- jsinfo/tasks.js
+++ jsinfo/tasks.js
@@ -119,13 +119,13 @@ let result;
 (a+b <4) ? result = "Below" : result = "Over";
 
 // task-15
-let messages = (login == 'Employee') ? 
-    'Hello' :
-(login == 'Director') ?
-    'Greetings' :
-(login == '') ?
-    'No login' : 
-    '';
+// let messages = (login == 'Employee') ? 
+//     'Hello' :
+// (login == 'Director') ?
+//     'Greetings' :
+// (login == '') ?
+//     'No login' : 
+//     '';
 
 // task-16
 alert(null || 2 && 3 || 4); // 3
@@ -169,3 +169,55 @@ while (true) {
 
 }
 alert('Sum: ' + sum);
+
+// task-20
+
+// let i = 0;
+// while (i++<5) console.log(i);
+let i = 0;
+while (++i<5) console.log(i);
+
+// task-21
+
+for ( let n=2; n <= 10; n++){
+    if(n % 2 == 0){
+        console.log(n)
+    }
+}
+
+// task-22
+
+for (let i = 0; i < 3; i++) {
+    alert(`number ${i}!`);
+}
+
+let j = 0;
+
+while(j<3){
+    alert(`number ${j}`);
+    j++;
+}
+
+// task-23
+
+let bigNumber;
+do {
+    bigNumber = prompt("Enter a number greater than 100?", 0)
+} while (bigNumber <= 100 && bigNumber);
+
+// task-24
+let n = prompt("Enter any limit", 0);
+
+for (let i =2; i<=n; i++){
+    let isPrime = true;
+
+    for (let j =2; j<i; j++){
+        if(i%j == 0){
+            isPrime = false;
+            break
+        }
+    }
+    if(isPrime){
+        alert(i)
+    }
+}
