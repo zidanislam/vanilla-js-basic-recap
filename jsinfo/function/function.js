fb847d1371c88ca317b03e21fe22fffeab0cde3a
Zidan Islam <zidantwinkle@gmail.com>
2025-03-05 21:59:09 +0600

    task related to function is completed

--- jsinfo/function/function.js
+++ jsinfo/function/function.js
@@ -1,20 +1,20 @@
 "use strict"
 
-// let from = prompt("name", "name");
-// let text = prompt("message", "message")
-// function showMessage(from, text) { // parameters: from, text
-//     alert(from + ': ' + text);
-// }
+let from = prompt("name", "name");
+let text = prompt("message", "message")
+function showMessage(from, text) { // parameters: from, text
+    alert(from + ': ' + text);
+}
 
-// showMessage(from, text); // Ann: Hello! (*)
+showMessage(from, text); // Ann: Hello! (*)
 
-// function showMessage2(from, text){
-//     from = "*" + from + "*";
-//     console.log(from+":" + text) 
-// }
-// let from2 = "Ann";
-// let text2 = "hello"
-// showMessage2(from2, text2);
+function showMessage2(from, text){
+    from = "*" + from + "*";
+    console.log(from+":" + text) 
+}
+let from2 = "Ann";
+let text2 = "hello"
+showMessage2(from2, text2);
 
 function pow(x,n){
     let result = x;
@@ -27,9 +27,55 @@ function pow(x,n){
 
 let x = prompt("input the number");
 let n = prompt("Input the power");
-pow(x,n);
-if(n<0){
+
+if(n<1){
     alert(`the value of ${n} is too low`);
 }else{
-    alert(pow(x,n))
+    alert(pow(x,n));
 }
+
+
+// function expression
+
+function sayHi() {
+    alert("Hello");
+}
+
+let func = sayHi;    
+func(); 
+sayHi();
+// 1
+function ask(question, yes, no){
+    if(confirm(question))yes();
+    else {no()};
+}
+
+
+let showYes = function(){
+    alert("you agreed!")
+}
+let showCancel = function(){
+    alert("you Canceled");
+}
+ask("Do you agree?", showYes, showCancel);
+
+//short form
+
+function ask(question, yes, no){
+    confirm(question) ? yes():no();
+}
+ask("Do you agree?",
+    function() {alert("You agreed");},
+    function() {alert("You canceled");}
+);
+
+//arrow function task
+
+let ask = (question, yes, no)=>{
+    confirm(question)? yes() : no()
+}
+
+ask("Do you agree",
+    ()=> alert("You agreed"),
+    ()=> alert("You Cancled")
+)
