0360cfd0f98f30feaf3eadc82f10de75997bbc1f
Zidan Islam <zidantwinkle@gmail.com>
2025-03-11 02:34:59 +0600

    object clone practice done

--- jsinfo/object-basic/object-basic.js
+++ jsinfo/object-basic/object-basic.js
@@ -53,3 +53,46 @@ function multiplyNumeric(obj) {
 
 multiplyNumeric(menu);
 console.log(menu);
+
+//cloning an object
+
+let user2 = {
+  name: "John",
+  age: 54,
+  height: 5.6,
+  weight: 70,
+};
+
+let clone = {};
+
+for (let key in user2) {
+  clone[key] = user2[key];
+}
+clone.name = "Max";
+clone.canDance = true;
+console.log(clone);
+// Object.assign();
+let permission1 = { canEat: true };
+let permission2 = { canDance: false };
+
+Object.assign(clone, permission1, permission2);
+console.log(clone);
+
+// Object.assign() clone;
+
+let clone2 = Object.assign({}, clone);
+console.log(clone2.name);
+
+// structuredClone
+
+const customer = {
+  name: "Leo",
+  age: 53,
+  size: {
+    height: 599,
+    width: 344,
+  },
+};
+let cloneCustomer = structuredClone(customer);
+cloneCustomer.size.height = 333;
+console.log(cloneCustomer.size.height);
