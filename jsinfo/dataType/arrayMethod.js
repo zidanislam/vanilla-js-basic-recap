a14836422614f1dc0eecd147e0a0fd28eb3fb57c
Zidan Islam <zidantwinkle@gmail.com>
2025-03-23 23:42:29 +0600

    few tasks of array methods done

--- jsinfo/dataType/arrayMethod.js
+++ jsinfo/dataType/arrayMethod.js
@@ -1,11 +1,11 @@
 "use strict";
 
-let arr = ["I", "study", "JavaScript", "right", "now"];
+// let arr = ["I", "study", "JavaScript", "right", "now"];
 
-const secondArr = arr.splice(0, 3, "Let's", "Damce");
-// arr.splice(0, 3);
-console.log(arr);
-console.log(secondArr);
+// const secondArr = arr.splice(0, 3, "Let's", "Damce");
+// // arr.splice(0, 3);
+// console.log(arr);
+// console.log(secondArr);
 
 const numArr = [1, 2];
 const newNumArr = numArr.concat(3, [5, 6, 7, 8], 4);
@@ -40,4 +40,45 @@ let name = users.filter(
   (user) => user.name.toLocaleLowerCase() == "John".toLocaleLowerCase()
 );
 users.sort((a, b) => a.id - b.id);
-console.log(name);
+users.sort((a, b) => b.id - a.id);
+
+console.log(users);
+let arrNumber = [1, 2, 3, 4, 5];
+
+let totalVal = arrNumber.reduce((sum, current) => sum + current);
+console.log(totalVal);
+console.log(Array.isArray(totalVal));
+console.log(Array.isArray(arrNumber));
+
+function camelize(str) {
+  return str
+    .split("-")
+    .map((word, index) =>
+      index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join("");
+}
+
+console.log(camelize("background-color"));
+
+function filterRangeInPlace(arr, a, b) {
+  arr.forEach((i) => a <= i <= b);
+  return arr;
+}
+let arr = [5, 3, 8, 1];
+
+filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
+
+console.log(arr);
+
+function copySorted(str) {
+  let sortedStr = str.slice().sort();
+  return sortedStr;
+}
+
+let arrText = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arrText);
+
+console.log(sorted); // CSS, HTML, JavaScript
+console.log(arrText);
