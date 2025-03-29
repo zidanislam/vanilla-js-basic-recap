f68eb2e8cc02cd3a384ffed85a6e22f117529090
Zidan Islam <zidantwinkle@gmail.com>
2025-03-29 21:42:44 +0600

    task solutions added

--- jsinfo/dataType/weakMap-weakSet.js
+++ jsinfo/dataType/weakMap-weakSet.js
@@ -1,1 +1,29 @@
 "use strict";
+
+let messages = [
+  { text: "Hello", from: "John" },
+  { text: "How goes?", from: "John" },
+  { text: "See you soon", from: "Alice" },
+];
+
+let readMessages = new WeakSet();
+
+// two messages have been read
+readMessages.add(messages[0]);
+readMessages.add(messages[1]);
+// readMessages has 2 elements
+
+// ...let's read the first message again!
+readMessages.add(messages[0]);
+// readMessages still has 2 unique elements
+
+// answer: was the message[0] read?
+alert("Read message 0: " + readMessages.has(messages[0])); // true
+
+messages.shift();
+
+// task-2
+
+let readMap = new WeakMap();
+
+readMap.set(messages[0], new Date(2017, 1, 1));
